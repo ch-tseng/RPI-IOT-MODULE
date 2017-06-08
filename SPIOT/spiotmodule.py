73d2f6efda435db79151b260251212ed2de4b702
ch-tseng <ch.tseng@sunplusit.com>
2017-06-08 18:46:01 +0800

    update

--- SPIOT/spiotmodule.py
+++ SPIOT/spiotmodule.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import time
+from threading import Thread
 import serial
 
 class SPIOT:
 
-    def query(self):
+    def printRawData(self):
         count = 1
         Serial = self.serial
 
@@ -16,14 +19,22 @@ class SPIOT:
     def noEncrypt(self):
         Serial = self.serial
         Serial.write(serial.to_bytes([0x99, 0x00, 0x00, 0x00, 0x00, 0x00]))
+        time.sleep(0.3)
 
     def queryDevices(self):
         Serial = self.serial
         Serial.write(serial.to_bytes([0x10, 0x00, 0x00, 0x00, 0x00, 0x00]))
+        time.sleep(0.3)
 
     def pushDevice(self):
         Serial = self.serial
         Serial.write(serial.to_bytes([0x08, 0x00, 0x00, 0x00, 0x00, 0x00]))
+        #time.sleep(0.3)
+
+    def removeAllDevice(self):
+        Serial = self.serial
+        Serial.write(serial.to_bytes([0x0F, 0xF0, 0x5A, 0xA5, 0x00, 0x00]))
+        time.sleep(0.3)
 
     def ByteToHex( self, byteStr ):
         """
@@ -39,7 +50,10 @@ class SPIOT:
         #
         #    return ''.join( hex ).strip()
 
-        return ''.join( [ "%02X " % ord( x ) for x in byteStr ] ).strip()
+        rtnValue = ''.join( [ "%02X " % ord( x ) for x in byteStr ] ).strip()
+        #print("Original:{} --> HEX:{}".format(byteStr, rtnValue))
+
+        return rtnValue
 
     def HexToByte( self, hexStr ):
         """
@@ -59,9 +73,95 @@ class SPIOT:
         for i in range(0, len(hexStr), 2):
             bytes.append( chr( int (hexStr[i:i+2], 16 ) ) )
 
-        return ''.join( bytes )
+        rtnValue = ''.join( bytes )
+        #print("Original:{} --> Byte:{}".format(hexStr, int(hexStr, 16)-64))
+
+        return rtnValue
+
+    def updateQueue(self):
+        queueRX = ['','','','','','']
+        stringRX = ""
+        Serial = self.serial
+      
+        if(Serial.in_waiting):   #讀取第一個byte
+            for line in Serial.read():
+                dataRX = self.ByteToHex(line)
+                stringRX = stringRX + dataRX
+                deviceData = self.dValue
+            
+                if(dataRX=='06'):
+                    queueRX[0] = dataRX
+ 
+                    if(Serial.in_waiting):   #讀取第二個byte
+                        for line in Serial.read():
+                            dataRX = self.ByteToHex(line)
+                            stringRX = stringRX + "-" + dataRX
+           
+                            if(dataRX[:1]=="3"):    #DOOR device
+                                deviceID = int(dataRX, 16)-48
+
+                                if(Serial.in_waiting):   #讀取第三個byte
+                                    for line in Serial.read():
+                                        dataRX = self.ByteToHex(line)
+                                        stringRX = stringRX + "-" + dataRX
+                                        print("deviceData={}, DOOR typeID={} , deviceID={}, value={}".format(deviceData, self.dType["DOOR"], deviceID, int(dataRX, 16)))
+
+                                        if self.dType["DOOR"] in deviceData:
+                                            deviceData[self.dType["DOOR"]][deviceID] = int(dataRX, 16)
+                                        else:
+                                            deviceData[self.dType["DOOR"]] = { deviceID: int(dataRX, 16) }  
+
+
+                            elif(dataRX[:1]=="4"):    #TH device
+                                deviceID = int(dataRX, 16)-64
+
+                                if(Serial.in_waiting):   #讀取第三個byte
+                                    for line in Serial.read():
+                                        dataRX = self.ByteToHex(line)
+                                        stringRX = stringRX + "-" + dataRX
+
+                                        print("deviceData={}, TH_T typeID={} , deviceID={}, value={}".format(deviceData, self.dType["TH_T"], deviceID, int(dataRX, 16)))
+
+                                        if self.dType["TH_T"] in deviceData:
+                                            deviceData[self.dType["TH_T"]][deviceID] = int(dataRX, 16)
+                                        else:
+                                            deviceData[self.dType["TH_T"]] = { deviceID: int(dataRX, 16) }
+
+
+                                        if(Serial.in_waiting):   #讀取第四個byte
+                                            for line in Serial.read():
+                                                dataRX = self.ByteToHex(line)
+                                                stringRX = stringRX + "-" + dataRX
+                                            
+                                                if self.dType["TH_H"] in deviceData:
+                                                    deviceData[self.dType["TH_H"]][deviceID] = int(dataRX, 16)
+                                                else:
+                                                    deviceData[self.dType["TH_H"]] = { deviceID: int(dataRX, 16) }
+
+                        self.dValue = deviceData
+                        print ("Received and coverted: {}".format(stringRX))
+                        print("self.dValue = {}".format(deviceData))
+
+                #elif(dataRX=='09'):
+
+
+        else:
+            self.pushDevice()
+            time.sleep(0.35)
+
+    def deviceTypeID(typeID=2):
+        typeList = self.dType
+        dTypeName = (key for key, value in typeList.items() if value == typeID).next()
+        return dTypeName
+
+    #def getData(self, tDevice, dID):
+        
 
     def __init__(self, baudrate=115200, portname='/dev/ttyAMA0', encrypt=False):
+        self.maxDeviceNum = 16
+        self.dType = {"PIR": 1, "DOOR": 2, "TH_T": 3, "TH_H": 4, "PLUG": 5 }
+        self.dValue = {}
+
         self.encrypt = encrypt
         self.baudrate = baudrate
         self.serial = serial.Serial(
@@ -72,8 +172,12 @@ class SPIOT:
             bytesize=serial.EIGHTBITS,
             timeout=1
             )
+
         if(encrypt==False):
             self.noEncrypt()
             time.sleep(0.5)
             self.queryDevices()
 
+    def begin(self):
+        background_thread = Thread(target=self.updateQueue())
+        background_thread.start()
--- demo.py
+++ demo.py
@@ -3,6 +3,14 @@ from SPIOT.spiotmodule import SPIOT
 
 iot = SPIOT(baudrate=115200, portname='/dev/ttyAMA0', encrypt=False)
 
+#iot.removeAllDevice()
+iot.begin()
+
+time.sleep(1)
+
 while True:
-    iot.query()
-    time.sleep(1)
+   
+    #iot.printRawData()
+    iot.updateQueue()
+
+    time.sleep(0.5)
