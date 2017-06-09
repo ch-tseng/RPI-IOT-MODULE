6abba42e8019932e503d90810b00d88c8d310f11
ch-tseng <ch.tseng@sunplusit.com>
2017-06-09 13:47:11 +0800

    update

--- demo.py
+++ demo.py
@@ -8,11 +8,15 @@ iot.begin()
 
 #time.sleep(1)
 
+iot.flashDevice("DOOR", 0)
+
 while True:
     #iot.printRawData()
     #iot.updateQueue()
     #print ("update")
     #iot.removeGroupDevices("PIR")
     print("DOOR --> {}".format(iot.getDeviceData("DOOR", 0)))
+    print("TH_T --> {}".format(iot.getDeviceData("TH_T", 0)))
+    print("TH_H --> {}".format(iot.getDeviceData("TH_H", 0)))
 
     time.sleep(0.5)
